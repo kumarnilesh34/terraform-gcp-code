47e29cd119c27e8779ee4818d8aa8b66aa138598
kumarnilesh34 <kumar.nilesh34@gmail.com>
2025-02-13 17:43:45 +0530

    Add ssh key and disk

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -17,4 +17,11 @@ resource "google_compute_instance" "instance_name" {
             // Ephemeral IP
         }
     }
+
+    metadata = {
+    ssh-keys = "user:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCLqdcpm9bjpx4CwAG1phzkHZdZCdn62NSkp2CH2D4/6agPFidIp95GMPoXEb4/+BrxqSn51FlzSxdBvUJfNXfvBuA8s41007dLxH3+SvGYk9clwzTNyyte+9I34l5ZoY/0WafWLWQXIj614eaYdmMbtiKRDS+zC5eykIzbh5d1QvVxLU/++5Y8QNfvN22vXN4Q+Egdz3J2h69XeFMwF+2JeWqKF7oTNkvRApIeeQ1m87PB709j5YL7jVo+dosuM+HzTYiLKJoAmsKQTVqA543QdtGDb2luWwFgOOQEMBX3rmpl9g6DbJpfWWCuZ7SbUFgE2jejMbwUq58XqcDO+h3t user"
+  }
+    lifecycle {
+    ignore_changes = [attached_disk]
+  }
 }
